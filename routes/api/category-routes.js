a5bddc68044dd204681f2d739d59cfd42300ec38
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-08 13:29:05 -0400

    Added GET route to find Category/Product(s) by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,16 +7,21 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: [Product],
-  })
-    .then((categories) => res.json(categories))
+    include: [Product]
+  }).then((categories) => res.json(categories))
     .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product]
+  }).then((category) => res.json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
