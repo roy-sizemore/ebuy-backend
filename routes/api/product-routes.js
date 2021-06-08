e408d8bcfb7cb5a427d6464ed7cb7a969c006e70
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-08 13:44:56 -0400

    Added GET route to find all Products w/Category and Tag data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      }
+    ]
+  }).then((products) => res.json(products))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
