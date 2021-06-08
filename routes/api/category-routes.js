87a3deb5eb605ccbb3830ee7ec1a6933747184a7
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-08 13:27:06 -0400

    Added Category.findAll() to find all Categories and include Products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,17 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
